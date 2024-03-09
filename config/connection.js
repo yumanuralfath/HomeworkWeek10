bdb69096dc868ee4a42070bcd24da8fe2137b0cb
Yumana <yumanuralfath2@gmail.com>
2024-03-09 09:44:41 +0700

    git init

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+import Sequelize from "sequelize";
+
+const db = new Sequelize(
+  "postgres://yumana:yumakeren@localhost:5432/hwrakaminmvc"
+);
+
+export default db;
---
+++ models/movies.js
@@ -0,0 +1,37 @@
+import { Sequelize, DataTypes } from "sequelize";
+import db from "../config/database.js";
+
+const movies = db.define(
+  "movies",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING(150),
+      allowNull: true,
+    },
+    genres: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+    },
+    year: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+    },
+    photo: {
+      type: '"CHAR"',
+      allowNull: true,
+    },
+  },
+  {
+    Sequelize,
+    tableName: "movies",
+    schema: "public",
+    timestamps: false,
+  }
+);
+
+export default movies;
